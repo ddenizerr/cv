d4fd705ec65239efdc7e90bc9ebfb3f2a8f8cd62
Deniz Denizer <ddenizer14@gmail.com>
2024-10-29 12:42:29 +0000

    cv work descriptions

--- resources/views/app/career/index.blade.php
+++ resources/views/app/career/index.blade.php
@@ -26,6 +26,14 @@
                         </div>
                         <div class="timeline-content">
                             <h5 class="title">Software Developer @ Apple</h5>
+                            <p class="description font-noto text-onyx fs-6">
+                                - Drive software development and project management across Cork and Distribution Centres, proactively managing the Shop Floor System to support high-volume manufacturing operations.
+                                <br>
+                                - Collaborating with cross-functional stakeholders (machine engineers, operations managers, and quality assurance teams) to gather requirements, aligning solutions with product introductions and process optimisation needs.
+                                <br>
+                                <strong> Key Technologies: </strong> PHP, Symfony, MySQL, Bootstrap.
+
+                            </p>
                         </div>
                     </div>
                     <!-- end experience section-->
@@ -42,7 +50,15 @@
                             </div>
                         </div>
                         <div class="timeline-content">
-                            <h5 class="title">Backend Developer @ MRM </h5>
+                            <h5 class="title">Backend Developer @ Manta Ray Media </h5>
+                            <p class="description font-noto text-onyx fs-6">
+                                - Maintained and improved Drupal-based websites and API services for various clients, ensuring high performance and reliability.
+                                <br>
+                                - Designed and styled custom content layouts according to specifications and user feedback, enhancing user experience.
+                                <br>
+                                <strong> Key Technologies: </strong> Symfony, jQuery, GraphQL, Drupal, Bootstrap, SCSS.
+
+                            </p>
                         </div>
                     </div>
                     <!-- end experience section-->
@@ -54,12 +70,20 @@
                             <div class="date-outer">
                                     <span class="date">
                                             <span class="month">1 Years</span>
-                                    <span class="year">2015</span>
+                                    <span class="year">2022</span>
                                     </span>
                             </div>
                         </div>
                         <div class="timeline-content">
                             <h5 class="title">Sofware Developer @ CIU </h5>
+                            <p class="description font-noto text-onyx fs-6">
+                                - Improved and maintained an admin portal for tracking Covid-19 cases and contacts, contributing to national public health initiatives.
+                                <br>
+                                - Developed critical modules, including the Patients Module, used by national airports and health authorities.
+                                <br>
+                                <strong>Key technologies:</strong> Laravel, JavaScript, Bootstrap, MariaDB, WAMP, Trello.
+
+                            </p>
                         </div>
                     </div>
                     <!-- end experience section-->
@@ -71,32 +95,22 @@
                             <div class="date-outer">
                                     <span class="date">
                                             <span class="month">2 Years</span>
-                                    <span class="year">2016</span>
+                                    <span class="year">2021</span>
                                     </span>
                             </div>
                         </div>
                         <div class="timeline-content">
-                            <h5 class="title">Backend Developer</h5>
+                            <h5 class="title">  Software Developer Engineer @ Analiz Systems	</h5>
+                            <p class="description font-noto text-onyx fs-6">
+                                - Designed and implemented a phone book application with CRUD operations, gaining practical experience in C# and .NET frameworks.
+                                <br>
+                                <strong> Key technologies: </strong> .NET Framework, C#, Bootstrap, CSS.
+
+                            </p>
                         </div>
                     </div>
                     <!-- end experience section-->
 
-                    <!-- start experience section-->
-                    <div class="timeline">
-                        <div class="icon"></div>
-                        <div class="date-content">
-                            <div class="date-outer">
-                                    <span class="date">
-                                            <span class="month">2 Years</span>
-                                    <span class="year">2018</span>
-                                    </span>
-                            </div>
-                        </div>
-                        <div class="timeline-content">
-                            <h5 class="title">Backend Developer</h5>
-                        </div>
-                    </div>
-                    <!-- end experience section-->
 
                 </div>
             </div>
