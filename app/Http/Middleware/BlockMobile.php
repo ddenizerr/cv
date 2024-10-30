af08bd3784a42b39d28c7b4bfa4320e6e00c4d5d
Deniz Denizer <ddenizer14@gmail.com>
2024-10-30 16:51:29 +0000

    middleware message fix

--- app/Http/Middleware/BlockMobile.php
+++ app/Http/Middleware/BlockMobile.php
@@ -19,7 +19,7 @@ class BlockMobile
         // Check if the request is coming from a mobile device
         if ($request->header('User-Agent') && preg_match('/Mobile|Android|iP(hone|ad)/i', $request->header('User-Agent'))) {
             // Return a custom response or redirect
-            return response('Mobile access is restricted.', 403);
+            return response('Mobile access is restricted. Yeah yeah, it will be available soon. Dont judge, work is so busy!', 403);
         }
 
         return $next($request);
